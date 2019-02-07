0f2a22fcd0a4fff56beda5e0e75cfa827301d29d
pablinchapin <pablinchapin@gmail.com>
2019-02-07 10:33:33 -0600

    Home Controller

--- picarto-admin/src/main/java/com/pablinchapin/picarto/admin/configuration/WebConfiguration.java
+++ picarto-admin/src/main/java/com/pablinchapin/picarto/admin/configuration/WebConfiguration.java
@@ -5,7 +5,12 @@
  */
 package com.pablinchapin.picarto.admin.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -15,5 +20,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+    
+    @Autowired
+    private MessageSource messageSource;
+     
+    
+    public Validator getValidator(){
+        
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        
+        factory.setValidationMessageSource(messageSource);
+    
+    return factory;
+    }
+    
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login")
+                    .setViewName("public/login");
+        
+        registry.addRedirectViewController("/", "/home");
+    }
     
 }
